c146e5449a7a14cd672e10f8e05ed32156d11990
zain alabeden <eng176zainalabeden@gmail.com>
2026-05-01 11:59:29 +0300

    updatee

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django_filters',
     'debug_toolbar',
     "django_spaghetti",
-    'rest_framework_jwt',
     'rest_framework_simplejwt.token_blacklist',
     'ECommerceApp'
 ]
